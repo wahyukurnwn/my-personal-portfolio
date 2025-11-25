4cdaeb0a496b9f7b6f912eb65de0ac430841f307
wahyukurnwn <whykurniaawan@gmail.com>
2025-11-25 16:16:00 +0700

    fixing link portfolio

--- src/data/index.ts
+++ src/data/index.ts
@@ -10,8 +10,8 @@ export const projects = [
     id: 1,
     title: "Expense Tracker App",
     des: "This Expense Tracker is a full-featured personal finance application designed to help users monitor their spending habits, categorize expenses, and gain better control over their financial activities.Built with a clean architecture and entity-driven backend structure, the system ensures scalability, clarity, and ease of future expansion.",
-    img: "/expensetracker.jpg",
+    img: "/expense-tracker-desktop.png",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/fm.svg", "/c.svg", "/git.svg"],
-    link: "https://github.com/wahyukurnwn?tab=repositories",
+    link: "https://expense-tracker-app-eosin-five.vercel.app/",
   },
 ];
